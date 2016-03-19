a25164474a72722b968373f8123743156647604b
AnCheTeng <ancheteng@AnCheTengde-MacBook-Pro.local>
2016-03-19 14:13:10 +0800

    Add ajax 1

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,30 @@
+$(document).ready(function() {
+  $.get("/physical", (response)=>{
+    $("#hospital-name").text(response.name);
+    $("#location").text("Taipei City");
+  });
 
+  $.get("/sickbed/number/specialbed", (response)=>{
+    $("#sickbed_o").text(response.number);
+  });
+
+  $.get("/patient/number/good", (response)=>{
+    $("#patient_o").text(response.number);
+  });
+
+  $.get("/healthcare/number/doctor", (response)=>{
+    $("#healthcare_o").text(response.number);
+  });
+
+  $.get("/drug/number/DrugA", (response)=>{
+    $("#drug_o").text(response.number);
+  });
+
+  $.get("/blood/number/A", (response)=>{
+    $("#blood_o").text(response.number);
+  });
+
+  $.get("/ambulance/number", (response)=>{
+    $("#ambulance_o").text(response.number);
+  });
+})
